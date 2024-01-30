190a791e6ea6966e03d3853eb513546d148f6484
davidfonsecagaona@gmail.com <davidfonsecagaona@gmail.com>
2024-01-30 11:24:45 -0600

    Adding last two problems

--- Module 3/m3p6.py
+++ Module 3/m3p6.py
@@ -2,4 +2,32 @@
 # 1/30/2024
 # CSS-220 Module 3 In-Class activity
 
-# 6 – Write a Python program to check if a set is a subset of another set. Use sets x = {apple, mango}, y = {mango, orange}, and z = {mango}. This program should use the issubset() method. 
+# Write a Python program to check if a set is a subset of another set. 
+# Use sets x = {apple, mango}, y = {mango, orange}, and z = {mango}. 
+# This program should use the issubset() method.
+
+# Create sets x, y, and z
+x = set(["apple", "mango"])
+y = set(["mango", "orange"])
+z = set(["mango"])
+print(x)
+print(y)
+print(z)
+
+print(f"Is x a subset of y?")
+print(x.issubset(y))
+
+print(f"Is x a subset of z?")
+print(x.issubset(z))
+
+print(f"Is y a subset of x?")
+print(y.issubset(x))
+
+print(f"Is y a subset of z?")
+print(y.issubset(z))
+
+print(f"Is z a subset of x?")
+print(z.issubset(x))
+
+print(f"Is z a subset of y?")
+print(z.issubset(y))
--- Module 3/m3p7.py
+++ Module 3/m3p7.py
@@ -2,4 +2,24 @@
 # 1/30/2024
 # CSS-220 Module 3 In-Class activity
 
-# 7 – Write a Python program to check if two given sets have no elements in common. Use sets x = {1, 2, 3, 4}, y = { 4, 5, 6, 7}, and z = {8}. This program should use the isdisjoint() method.
+# Write a Python program to check if two given sets have no elements in common. 
+#Use sets x = {1, 2, 3, 4}, y = { 4, 5, 6, 7}, and z = {8}. 
+# This program should use the isdisjoint() method.
+
+# Create sets x, y, and z
+x = set([1, 2, 3, 4])
+y = set([4, 5, 6, 7])
+z = set([8])
+print(x)
+print(y)
+print(z)
+
+# Chekc if sets are disjoint
+print("Are x and y disjoint?")
+print(x.isdisjoint(y))
+
+print("Are x and z disjoint?")
+print(x.isdisjoint(z))
+
+print("Are y and y disjoint?")
+print(y.isdisjoint(z))
